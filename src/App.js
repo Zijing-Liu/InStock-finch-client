1dd83a87ea736d2cf422458cb081fd2995ca1285
Bethany Cannon <bethany.cannon@student.ufv.ca>
2024-03-12 14:36:40 -0700

    set up hard code for styling while backend is being developed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import WarehouseList from "../src/component/WarehouseList/WarehouseList"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Welcome</header>
+      <WarehouseList />
     </div>
   );
 }
--- src/component/WarehouseList/WarehouseList.jsx
+++ src/component/WarehouseList/WarehouseList.jsx
@@ -1,36 +1,48 @@
 import "./WarehouseList.scss";
 import SearchLogo from "../../assets/Icons/search-24px.svg"
 
-function WarehouseList(){
-return(
-    <section>
-        <div>
-            <h1>Warehouses</h1>
+function WarehouseList() {
+    return (
+        <section>
             <div>
-            <input className="" type="text" name="search-bar" placeholder="Search"></input>
-            <button>
-                <img src={SearchLogo} />
-            </button>
-            </div>
-            <button>+Add a New Warehouse</button>
-        </div>
-        {warehouseListData.Map((warehouse)=> {
-            return(
-                <div key={warehouse.id}>
-                    <h2>Warehouse</h2>
-                    {/* Link/navigate to warehouseDetails */}
-                    <p>{warehouse.name}</p>
-                    <h2>Address</h2>
-                    <p>{warehouse.address} {warehouse.city} {warehouse.country}</p>
-                    <h2>Contact Name</h2>
-                    <p>{warehouse.contact_name}</p>
-                    <h2>Contact Information</h2>
-                    <p>{warehouse.contact_phone} {warehouse.contact_email}</p>
+                <h1>Warehouses</h1>
+                <div>
+                    <input className="" type="text" name="search-bar" placeholder="Search"></input>
+                    <button>
+                        <img src={SearchLogo} />
+                    </button>
                 </div>
-            )}
-        )}
-    </section>
-)
+                <button>+Add a New Warehouse</button>
+            </div>
+            <div>
+                <h2>Warehouse</h2>
+                {/* Link/navigate to warehouseDetails */}
+                <p>Test Warehouse</p>
+                <h2>Address</h2>
+                <p>123 street, Test City, Canada</p>
+                <h2>Contact Name</h2>
+                <p>John Doe</p>
+                <h2>Contact Information</h2>
+                <p>123-456-7890 abc@test.xyz</p>
+            </div>
+        </section>
+    )
 }
 
-export default WarehouseInventory;
+//        {warehouseListData.Map((warehouse)=> {
+//     return(
+//         <div key={warehouse.id}>
+//             <h2>Warehouse</h2>
+//             {/* Link/navigate to warehouseDetails */}
+//             <p>{warehouse.name}</p>
+//             <h2>Address</h2>
+//             <p>{warehouse.address} {warehouse.city} {warehouse.country}</p>
+//             <h2>Contact Name</h2>
+//             <p>{warehouse.contact_name}</p>
+//             <h2>Contact Information</h2>
+//             <p>{warehouse.contact_phone} {warehouse.contact_email}</p>
+//         </div>
+//     )}
+// )}
+
+export default WarehouseList;
