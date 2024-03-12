1a14c254598bb7b07156e7d484ac4e54535c4a3e
ghofran <gho0ofran.adel14@gmail.com>
2024-03-12 13:56:09 -0700

    Install all required NPM packages for React routing and set up the basic routes needed for this project. Create placeholder components to use for each route.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Warehouses from "./pages/Warehouses/Warehouses";
+import Inventory from "./pages/Inventory/Inventory";
+import InventoryDetails from "./pages/InventoryDetails/InventoryDetails";
+import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">Welcome</header>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path="/Warehouses" element={<Warehouses />} />
+        <Route path="/Inventory" element={<Inventory />} />
+        <Route path="/Inventory/:ID" element={<InventoryDetails />} />
+        <Route path="/Warehouses/:ID" element={<WarehouseDetails />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
