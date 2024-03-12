bcab38c4a3cdd1757e6f4ca5a2f5a3e8b28b9c86
Celine Liu <mangolzj@gmail.com>
2024-03-12 15:37:20 -0400

    deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Welcome</header>
     </div>
   );
 }
