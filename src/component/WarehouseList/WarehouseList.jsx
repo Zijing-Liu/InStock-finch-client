acb3f07176f608b2a025439c67131e22bd71fc9a
Bethany Cannon <bethany.cannon@student.ufv.ca>
2024-03-13 12:49:27 -0700

    modify styling of tablet h2

--- src/component/WarehouseList/WarehouseList.jsx
+++ src/component/WarehouseList/WarehouseList.jsx
@@ -19,70 +19,66 @@ function WarehouseList() {
             <div className="list__main-container">
                 <h1>{warehouseObj.title}</h1>
                 <div className="list__search-spacing">
-                <div className="list__search-container">
-                    <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
-                    <button className="list__search-btn">
-                        <img src={searchLogo} className="list__search-img"/>
-                    </button>
-                </div>
-                <button className="list__btn">{warehouseObj.button}</button>
+                    <div className="list__search-container">
+                        <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
+                        <button className="list__search-btn">
+                            <img src={searchLogo} className="list__search-img" />
+                        </button>
+                    </div>
+                    <button className="list__btn">{warehouseObj.button}</button>
                 </div>
             </div>
 
             <div className="list__header">
                 <div className="list__icons list__warehouse">
-                <h2>Warehouse</h2> <img src={sort}/>
+                    <h2>Warehouse</h2> <img src={sort} />
                 </div>
                 <div className="list__icons list__address">
-                <h2>Address</h2> <img src={sort}/>
+                    <h2>Address</h2> <img src={sort} />
                 </div>
                 <div className="list__icons list__name">
-                <h2>Contact Name</h2> <img src={sort}/>
+                    <h2>Contact Name</h2> <img src={sort} />
                 </div>
                 <div className="list__icons list__info">
-                <h2>Contact Information</h2> <img src={sort}/>
+                    <h2>Contact Information</h2> <img src={sort} />
                 </div>
                 <h2 className="list__action">Actions</h2>
             </div>
-            {data.map((warehouse)=> {
-                return(
-                    <div key={warehouse.id}>
-            <div className="list__warehouse-container">
-                <Link to="warehouse/:id" className="list__warehouse">
-                <div className="list__icons list__icons--active">
-                    <p>{warehouse.warehouse_name}</p> <img src={chevron}/>
+
+            <div className="list__mobile-container">
+                <div className="list__warehouse-info">
+                    <h2 className="list__header-mobile">Warehouse</h2>
+                    <Link to="warehouse/:id" className="list__warehouse">
+                        <div className="list__icons">
+                            <p className="list__address">Test Warehouse</p> <img src={chevron} />
+                        </div>
+                    </Link>
+                    <h2 className="list__header-mobile">Address</h2>
+                    <p>123 street, <br /> Test City, Canada</p>
+                    <img className="list__btn-mobile" src={deleteBtn} />
                 </div>
-                </Link>
-                <p className="list__address">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
-                <p className="list__name">{warehouse.contact_name}</p>
-                <p className="list__info">{warehouse.contact_phone} <br /> {warehouse.contact_email}</p>
+                <div className="list__warehouse-contact">
+                    <h2 className="list__header-mobile">Contact Name</h2>
+                    <p>John Doe</p>
+                    <h2 className="list__header-mobile">Contact Information</h2>
+                    <p>123-456-7890 <br /> abc@test.xyz</p>
+                    <img className="list__btn-mobile" src={editBtn} />
+                </div>
                 <div className="list__list-btns"> <img src={deleteBtn} /> <img src={editBtn} /> </div>
             </div>
 
-            <div className="list__warehouse-container-mobile">
-                <div className="list__warehouse-info">
-                <h2 className="list__header-mobile">Warehouse</h2>
+            <div className="list__warehouse-container">
                 <Link to="warehouse/:id" className="list__warehouse">
-                    <div className="list__icons">
-                <p className="list__address">Test Warehouse</p> <img src={chevron}/>
-                </div>
+                    <div className="list__icons list__icons--active">
+                        <p>warehouse.warehouse_name</p> <img src={chevron} />
+                    </div>
                 </Link>
-                <h2 className="list__header-mobile">Address</h2>
-                <p>123 street, <br /> Test City, Canada</p>
-                <img className="list__btn-mobile" src={deleteBtn} />
-                </div>
-                <div className="list__warehouse-contact">
-                <h2 className="list__header-mobile">Contact Name</h2>
-                <p>John Doe</p>
-                <h2 className="list__header-mobile">Contact Information</h2>
-                <p>123-456-7890 <br /> abc@test.xyz</p>
-                <img className="list__btn-mobile" src={editBtn} />
-                </div>
+                <p className="list__address">warehouse.address, warehouse.city, warehouse.country</p>
+                <p className="list__name">warehouse.contact_name</p>
+                <p className="list__info">warehouse.contact_phone <br /> warehouse.contact_email</p>
                 <div className="list__list-btns"> <img src={deleteBtn} /> <img src={editBtn} /> </div>
             </div>
-            </div>
-                            )
-                        })}
+
         </section>
     )
 }
