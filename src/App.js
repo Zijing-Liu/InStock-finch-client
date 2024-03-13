e122b3803d95d8d7c0ee7e900bf80d3f910ff31b
Melanie <melanie.rawluk@gmail.com>
2024-03-13 07:57:24 -0600

    Add warehouse form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from "./component/Header"
+import Header from "./component/Header";
+import EditWarehouse from "./component/EditWarehouse/EditWarehouse";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <EditWarehouse />
     </div>
   );
 }
---
+++ src/component/EditWarehouse/EditWarehouse.jsx
@@ -0,0 +1,45 @@
+import './EditWarehouse.scss';
+
+export default function EditWarehouse() {
+
+
+    // Form validation
+
+
+    return (
+        <section className='edit-warehouse'>
+            <h1 className='edit-warehouse__title'>Edit Warehouse</h1>
+            <form action="" className='edit-warehouse__form'>
+                <div className='edit-warehouse__form-fields'>
+                    <div className='edit-warehouse__form-section'>
+                        <h2 className='edit-warehouse__form-heading'>Warehouse Details</h2>
+                        <label htmlFor="warehouse-name" className='edit-warehouse__form-label'>Warehouse Name</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="street-address" className='edit-warehouse__form-label'>Street Address</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="city" className='edit-warehouse__form-label'>City</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="country" className='edit-warehouse__form-label'>Country</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                    </div>
+                    <div className='edit-warehouse__form-section edit-warehouse__form-section--contact'>
+                        <h2 className='edit-warehouse__form-heading'>Contact Details</h2>
+                        <label htmlFor="contact-name" className='edit-warehouse__form-label'>Contact Name</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="position" className='edit-warehouse__form-label'>Position</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="phone-number" className='edit-warehouse__form-label'>Phone Number</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                        <label htmlFor="email" className='edit-warehouse__form-label'>Email</label>
+                        <input type="text" className='edit-warehouse__form-input' />
+                    </div>
+                </div>
+                <div className='edit-warehouse__form-buttons'>
+                    <button className='edit-warehouse__form-buttons--cancel'>Cancel</button>
+                    <button className='edit-warehouse__form-buttons--save' type='submit'>Save</button>
+                    {/* For cancel button: onClick={() => navigate('/') */}
+                </div>
+            </form>
+        </section>
+    )
+}
