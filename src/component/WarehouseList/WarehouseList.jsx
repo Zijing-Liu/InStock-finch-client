fa0a0466171ac10652f17f9755d0da351d7e9280
Bethany Cannon <bethany.cannon@student.ufv.ca>
2024-03-13 18:55:36 -0700

    repair styling and add psudeo classes

--- src/component/WarehouseList/WarehouseList.jsx
+++ src/component/WarehouseList/WarehouseList.jsx
@@ -12,19 +12,21 @@ function WarehouseList() {
         button: "+ Add New Warehouse"
     }
 
+    function clickHandler() {
+        alert("I've been clicked!")
+    }
+
     return (
         <section className="list">
             <div className="list__main-container">
                 <h1>{warehouseObj.title}</h1>
-                <div className="list__search-spacing">
-                    <div className="list__search-container">
-                        <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
-                        <button className="list__search-btn">
-                            <img src={searchLogo} className="list__search-img" />
-                        </button>
-                    </div>
-                    <button className="list__btn">{warehouseObj.button}</button>
+                <div className="list__search-container">
+                    <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
+                    <button className="list__search-btn">
+                        <img src={searchLogo} className="list__search-img" />
+                    </button>
                 </div>
+                <button className="list__btn">{warehouseObj.button}</button>
             </div>
 
             <div className="list__header">
@@ -53,28 +55,34 @@ function WarehouseList() {
                     </Link>
                     <h2 className="list__header-mobile">Address</h2>
                     <p>123 street, <br /> Test City, Canada</p>
-                    <img className="list__btn-mobile" src={deleteBtn} />
+                    <button className="list__btn-mobile" onClick={clickHandler}> <img src={deleteBtn} /> </button>
                 </div>
                 <div className="list__warehouse-contact">
                     <h2 className="list__header-mobile">Contact Name</h2>
                     <p>John Doe</p>
                     <h2 className="list__header-mobile">Contact Information</h2>
                     <p>123-456-7890 <br /> abc@test.xyz</p>
-                    <img className="list__btn-mobile" src={editBtn} />
+                    <button className="list__btn-mobile" onClick={clickHandler}> <img src={editBtn} /> </button>
                 </div>
-                <div className="list__list-btns"> <img src={deleteBtn} /> <img src={editBtn} /> </div>
+                <Link to="/warehouses/edit/:ID" className="list__list-btns">
+                    <img src={editBtn} />
+                </Link>
             </div>
 
             <div className="list__warehouse-container">
                 <Link to="warehouse/:id" className="list__warehouse">
                     <div className="list__icons list__icons--active">
-                        <p>warehouse.warehouse_name</p> <img src={chevron} />
+                        <p className="list__address">Test Warehouse</p> <img src={chevron} />
                     </div>
                 </Link>
-                <p className="list__address">warehouse.address, warehouse.city, warehouse.country</p>
-                <p className="list__name">warehouse.contact_name</p>
-                <p className="list__info">warehouse.contact_phone <br /> warehouse.contact_email</p>
-                <div className="list__list-btns"> <img src={deleteBtn} /> <img src={editBtn} /> </div>
+                <p className="list__address">123 street, Test City, Canada</p>
+                <p className="list__name"> John Doe</p>
+                <p className="list__info">123-456-7890 <br /> abc@test.xyz</p>
+                <div className="list__list-btns"> <button onClick={clickHandler}><img src={deleteBtn} /></button>
+                    <Link to="/warehouses/edit/:ID">
+                        <img src={editBtn} />
+                    </Link>
+                </div>
             </div>
 
         </section>
