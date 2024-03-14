7bd758c1af35e9ae7353e37a4d90b41ab5d64911
ghofran <gho0ofran.adel14@gmail.com>
2024-03-14 12:23:15 -0700

    adding a sprate form compenent that can be used in both edit and post pasges

---
+++ src/component/InventoryForm/InventoryForm.jsx
@@ -0,0 +1,252 @@
+// import "/InventoryForm.scss";
+import { useEffect } from "react";
+
+function InventoryForm({
+  handleOnSubmit,
+  formData,
+  handleOnClick,
+  setFormData,
+  setGetFormValidation,
+  getFormValidation,
+  setError,
+  error,
+}) {
+  const Warehouses = ["Manhattan", "Washington", "San Francisco"];
+  const categories = [
+    "Apparel",
+    "Accessories",
+    "Electronics",
+    "Electronics",
+    "Gear",
+    "Health",
+  ];
+
+  // handle inputs change
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    // If status is "Out Of Stock", set quantity to 0
+    if (name === "status" && value === "Out Of Stock") {
+      setFormData({
+        ...formData,
+        quantity: 0,
+        [name]: value,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+  console.log(formData);
+
+  // Set the form validation function to the state
+  useEffect(() => {
+    // check form validation
+    const isFormValid = () => {
+      let isValid = true;
+      const errors = {};
+
+      if (formData.item_name.length < 2) {
+        errors.item_name = "This field is required";
+        isValid = false;
+      }
+      if (formData.description.length < 2) {
+        errors.description = "This field is required";
+        isValid = false;
+      }
+      if (formData.quantity < 0) {
+        errors.quantity = "Invalid input";
+        isValid = false;
+      }
+      // Update the error state
+      setError(errors);
+      // Update the getFormValidation state
+      setGetFormValidation(isValid);
+
+      return isValid;
+    };
+    isFormValid();
+  }, [formData, setGetFormValidation, setError]);
+
+  return (
+    <>
+      <form className="form" onSubmit={handleOnSubmit}>
+        <div className="form__container">
+          <div className="form__box">
+            <h3 className="form__title">Item Details</h3>
+
+            <label htmlFor="name" className="form__label">
+              Item Name
+            </label>
+            <input
+              className={
+                error.item_name
+                  ? ` form__input form__input--red`
+                  : `form__input`
+              }
+              type="text"
+              placeholder={formData.item_name}
+              id="name"
+              name="item_name"
+              value={formData.item_name}
+              onChange={handleInputChange}
+            />
+            {error.item_name && (
+              <p className="form__input--error">{error.item_name}</p>
+            )}
+
+            <label htmlFor="description" className="form__label">
+              Description
+            </label>
+            <textarea
+              className={
+                error.description
+                  ? ` form__input form__input--text form__input--red`
+                  : `form__input form__input--text`
+              }
+              placeholder={formData.description}
+              id="description"
+              name="description"
+              rows=""
+              value={formData.description}
+              onChange={handleInputChange}
+            ></textarea>
+            {error.description && (
+              <p className="form__input--error">{error.description}</p>
+            )}
+
+            <label htmlFor="category" className="form__label">
+              Category
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="form__input form__input--select"
+              value={formData.category}
+              onChange={handleInputChange}
+            >
+              {categories.map((category, i) => (
+                <option key={i} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <hr />
+
+          <div className="form__box">
+            <h3 className="form__title">Item Availability</h3>
+            <label htmlFor="status" className="form__label">
+              Status
+            </label>
+            <div className="form__status">
+              <div
+                className={
+                  formData.status === "In Stock"
+                    ? "form__check form__check--checked"
+                    : "form__check"
+                }
+              >
+                <input
+                  type="radio"
+                  id="in-stock"
+                  name="status"
+                  value="In Stock"
+                  checked={formData.status === "In Stock"}
+                  onChange={handleInputChange}
+                />
+                <label
+                  htmlFor="in-stock"
+                  className="form__label form__label--stock"
+                >
+                  In Stock
+                </label>
+              </div>
+
+              <div
+                className={
+                  formData.status === "Out Of Stock"
+                    ? "form__check form__check--checked"
+                    : "form__check"
+                }
+              >
+                <input
+                  type="radio"
+                  id="out-of-stock"
+                  name="status"
+                  value="Out Of Stock"
+                  checked={formData.status === "Out Of Stock"}
+                  onChange={handleInputChange}
+                />
+                <label
+                  htmlFor="out-of-stock"
+                  className="form__label form__label--stock"
+                >
+                  Out Of Stock
+                </label>
+              </div>
+            </div>
+            {formData.status === "In Stock" && (
+              <>
+                <label htmlFor="quantity" className="form__label">
+                  Quantity
+                </label>
+                <input
+                  className={
+                    error.quantity
+                      ? "form__input form__input--red"
+                      : "form__input"
+                  }
+                  type="number"
+                  placeholder={formData.quantity}
+                  id="quantity"
+                  name="quantity"
+                  value={formData.quantity}
+                  onChange={handleInputChange}
+                />
+                {error.quantity && (
+                  <p className="form__input--error">{error.quantity}</p>
+                )}
+              </>
+            )}
+
+            <label htmlFor="warehouse" className="form__label">
+              Warehouse
+            </label>
+            <select
+              id="warehouse"
+              name="warehouse_id"
+              className="form__input form__input--select"
+              value={formData.warehouse_id}
+              onChange={handleInputChange}
+            >
+              {Warehouses.map((warehouse, i) => (
+                <option key={i} value={warehouse}>
+                  {warehouse}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className="form__btn-box">
+          <button
+            type="button"
+            className="form__btn form__btn--white"
+            onClick={handleOnClick}
+          >
+            Cancel
+          </button>
+          <button type="submit" className="form__btn">
+            Save
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default InventoryForm;
--- src/pages/EditInventory/EditInventory.jsx
+++ src/pages/EditInventory/EditInventory.jsx
@@ -3,20 +3,13 @@ import arrowIcon from "../../assets/Icons/arrow_back-24px.svg";
 import "./EditInventory.scss";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import InventoryForm from "../../component/InventoryForm/InventoryForm";
 
 function EditInventory() {
   const { ID } = useParams();
 
   // mock data to test
-  const Warehouses = ["Manhattan", "Washington", "San Francisco"];
-  const categories = [
-    "Apparel",
-    "Accessories",
-    "Electronics",
-    "Electronics",
-    "Gear",
-    "Health",
-  ];
+
   const initalInventory = {
     id: 1,
     warehouse_id: 2,
@@ -29,55 +22,19 @@ function EditInventory() {
   };
 
   const [formData, setFormData] = useState(initalInventory);
+
+  const [getFormValidation, setGetFormValidation] = useState(false);
+
   const [error, setError] = useState({});
-
-  // handle inputs change
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-
-    // If status is "Out Of Stock", set quantity to 0
-    if (name === "status" && value === "Out Of Stock") {
-      setFormData({
-        ...formData,
-        quantity: 0,
-        [name]: value,
-      });
-    } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    }
-  };
-console.log(formData)
-  // check form validation
-  const isFormValid = () => {
-    let isValid = true;
-
-    const errors = {};
-
-    if (formData.item_name.length < 2) {
-      errors.item_name = "This field is required";
-      isValid = false;
-    }
-    if (formData.description.length < 2) {
-      errors.description = "This field is required";
-      isValid = false;
-    }
-    if (formData.quantity < 0) {
-      errors.quantity = "Invalid input";
-      isValid = false;
-    }
-    // Update the error state
-    setError(errors);
-
-    return isValid;
-  };
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
+    console.log('i am clicked');
+    console.log(getFormValidation)
     // check validation
-    isFormValid();
+    if (!getFormValidation) {
+      return;
+    }
 
     // update data
     // try {
@@ -106,164 +63,17 @@ console.log(formData)
         <h2>Edit Inventory Item</h2>
       </div>
       <hr />
+      <InventoryForm
+        setFormData={setFormData}
+        formData={formData}
+        handleOnClick={handleOnClick}
+        handleOnSubmit={handleOnSubmit}
+        setGetFormValidation={setGetFormValidation}
+        getFormValidation={getFormValidation}
+        error={error}
+        setError={setError}
 
-      <form className="form" onSubmit={handleOnSubmit}>
-        <div className="form__container">
-          <div className="form__box">
-            <h3 className="form__title">Item Details</h3>
-
-            <label htmlFor="name" className="form__label">
-              Item Name
-            </label>
-            <input
-              className="form__input"
-              type="text"
-              placeholder={formData.item_name}
-              id="name"
-              name="item_name"
-              value={formData.item_name}
-              onChange={handleInputChange}
-            />
-            {error.item_name && (
-              <p className="form__input--error">{error.item_name}</p>
-            )}
-
-            <label htmlFor="description" className="form__label">
-              Description
-            </label>
-            <textarea
-              className={
-                error.description
-                  ? ` form__input form__input--text form__input--red`
-                  : `form__input form__input--text`
-              }
-              placeholder={formData.description}
-              id="description"
-              name="description"
-              rows=""
-              value={formData.description}
-              onChange={handleInputChange}
-            ></textarea>
-            {error.description && (
-              <p className="form__input--error">{error.description}</p>
-            )}
-
-            <label htmlFor="category" className="form__label">
-              Category
-            </label>
-            <select
-              id="category"
-              name="category"
-              className="form__input form__input--select"
-              value={formData.category}
-              onChange={handleInputChange}
-            >
-              {categories.map((category, i) => (
-                <option key={i} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <hr />
-
-          <div className="form__box">
-            <h3 className="form__title">Item Availability</h3>
-            <label htmlFor="status" className="form__label">
-              Status
-            </label>
-            <div className="form__status">
-              <div className={formData.status === "In Stock" ? "form__check form__check--checked" : "form__check" }>
-                <input
-                  type="radio"
-                  id="in-stock"
-                  name="status"
-                  value="In Stock"
-                  checked={formData.status === "In Stock"}
-                  onChange={handleInputChange}
-                />
-                <label
-                  htmlFor="in-stock"
-                  className="form__label form__label--stock"
-                >
-                  In Stock
-                </label>
-              </div>
-
-              <div className={formData.status === "Out Of Stock" ? "form__check form__check--checked" : "form__check" }>
-                <input
-                  type="radio"
-                  id="out-of-stock"
-                  name="status"
-                  value="Out Of Stock"
-                  checked={formData.status === "Out Of Stock"}
-                  onChange={handleInputChange}
-                />
-                <label
-                  htmlFor="out-of-stock"
-                  className="form__label form__label--stock"
-                >
-                  Out Of Stock
-                </label>
-              </div>
-            </div>
-            {formData.status === "In Stock" && (
-              <>
-                <label htmlFor="quantity" className="form__label">
-                  Quantity
-                </label>
-                <input
-                  className={
-                    error.quantity
-                      ? "form__input form__input--red"
-                      : "form__input"
-                  }
-                  type="number"
-                  placeholder={formData.quantity}
-                  id="quantity"
-                  name="quantity"
-                  value={formData.quantity}
-                  onChange={handleInputChange}
-                />
-                {error.quantity && (
-                  <p className="form__input--error">{error.quantity}</p>
-                )}
-              </>
-            )}
-
-            <label htmlFor="warehouse" className="form__label">
-              Warehouse
-            </label>
-            <select
-              id="warehouse"
-              name="warehouse_id"
-              className="form__input form__input--select"
-              value={formData.warehouse_id}
-              onChange={handleInputChange}
-            >
-              {Warehouses.map((warehouse, i) => (
-                <option key={i} value={warehouse}>
-                  {warehouse}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="form__btn-box">
-          <button
-            type="button"
-            className="form__btn form__btn--white"
-            onClick={handleOnClick}
-          >
-            Cancel
-          </button>
-          <button type="submit" className="form__btn">
-            Save
-          </button>
-        </div>
-      </form>
+      />
     </div>
   );
 }
