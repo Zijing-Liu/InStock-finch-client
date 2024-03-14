d60e3887da5062cbc9d3fab7c7511626c03686a2
Celine Liu <mangolzj@gmail.com>
2024-03-14 14:09:49 -0400

    starter code for building the add-inventory page

--- src/pages/AddInventory/AddInventory.jsx
+++ src/pages/AddInventory/AddInventory.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./AddInventory.scss";
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
 function AddInventory() {
   return (
-    <div>AddInventory</div>
-  )
+    <div className="add-inventory">
+      <div className="add-inventory__header">
+        <div className="add-inventory__iconAndTitle">
+          <img src={backArrow} alt="" />
+          <h1 className="add-inventory__heading">Add New Inventory Item</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddInventory
+export default AddInventory;
