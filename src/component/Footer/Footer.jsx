589b3ae574c5ed5486017d7e0ec6e9060baf803f
adams-infinity <lindsayadamsdesign@gmail.com>
2024-03-15 12:15:50 -0400

    add footer.jsx and footer.scss

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.scss'
+
+
+export default function Footer() {
+
+    return (
+        <footer className='footer'>
+            <h4>© InStock Inc. All Rights Reserved.</h4>
+        </footer>
+    )
+
+}
+
