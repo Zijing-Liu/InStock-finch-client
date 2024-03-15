676276a52f9d6c7ea6c252f93eaf17e53c92fccf
adams-infinity <lindsayadamsdesign@gmail.com>
2024-03-15 11:15:03 -0400

    copy paste code from warehouseList page

---
+++ src/component/InventoryList/InventoryList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Inventory.scss";
+import searchLogo from "../../assets/Icons/search-24px.svg";
+import sort from "../../assets/Icons/sort-24px.svg"
+import { useState } from "react";
+import WarehouseCard from "../WarehouseCard/WarehouseCard";
+
+
+function Inventory({warehouseData}) {
+
+    const inventortyObj = {
+        title: "Inventory",
+        button: "+ Add New Item",
+    }
+
+
+    return (
+        <section>
+            <div className="list">
+                <div className="list__main-container">
+                    <h1>{inventoryObj.title}</h1>
+                    <div className="list__search-container">
+                        <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
+                        <button className="list__search-btn">
+                            <img src={searchLogo} className="list__search-img" />
+                        </button>
+                    </div>
+                    <button className="list__btn">{inventoryObj.button}</button>
+                </div>
+
+        {warehouseData.map((warehouse) => {
+            return(
+                <WarehouseCard 
+                warehouse={warehouse}
+                
+                key={warehouse.id}
+                />
+            )}
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default Inventory;
--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import InventoryList from "../../component/InventoryList/InventoryList"
+import axios from "axios";
+import { useState, useEffect } from "react"
 
-function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+export default function Warehouses() {
+const baseURL = process.env.REACT_APP_BASE_URL
+const [inventoryData, setInventoryData] = useState(null)
+
+useEffect(() => {
+  const inventoryData = async() => {
+    try{
+      const data = await axios.get(`${baseURL}warehouses`)
+      setInventoryData(data.data)
+    }catch(error){
+      console.log("inventory list data error")
+    }
+  }
+  warehouseData();
+}, []);
+
+
+if (!warehouseData) {
+  return <div>loading...</div>;
 }
 
-export default Inventory
+return (
+  <div>
+    <div>Warehouses</div>
+    <div>
+      <WarehouseList warehouseData={warehouseData} />
+    </div>
+  </div>
+);
+}
