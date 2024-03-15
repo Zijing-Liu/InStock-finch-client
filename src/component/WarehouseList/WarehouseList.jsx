a3352a1fc0b0b73950976ddec1ea25c89d8a00c2
Bethany Cannon <bethany.cannon@student.ufv.ca>
2024-03-15 06:47:46 -0700

    updated warehouse list tp make components and styling easier to reuse

--- src/component/WarehouseList/WarehouseList.jsx
+++ src/component/WarehouseList/WarehouseList.jsx
@@ -1,20 +1,19 @@
 import "./WarehouseList.scss";
 import searchLogo from "../../assets/Icons/search-24px.svg";
-import deleteBtn from "../../assets/Icons/delete_outline-24px.svg"
-import editBtn from "../../assets/Icons/edit-24px.svg"
-import chevron from "../../assets/Icons/chevron_right-24px.svg"
 import sort from "../../assets/Icons/sort-24px.svg"
-import { Link } from "react-router-dom";
+import { useState } from "react";
+// import { useParams } from "react-router-dom";
+import DeleteWarehouse from "../DeleteWarehouse/DeleteWarehouse";
+import WarehouseCard from "../WarehouseCard/WarehouseCard";
+
 
 function WarehouseList({warehouseData}) {
 
     const warehouseObj = {
         title: "Warehouses",
         button: "+ Add New Warehouse",
-        id: 1
     }
 
-    console.log(warehouseData)
 
     return (
         <section>
@@ -46,55 +45,13 @@ function WarehouseList({warehouseData}) {
                     <h2 className="list__action">Actions</h2>
                 </div>
         {warehouseData.map((warehouse) => {
-            {console.log(warehouse)}
             return(
-                <div key={warehouse.id}>
-                    <div className="list__mobile-container">
-                        <div className="list__mobile-warehouse">
-                            <h2 className="list__header-mobile">Warehouse</h2>
-                            <Link to={`/warehouses/${warehouse.id}`} className="list__warehouse">
-                                <div className="list__icons">
-                                    <p className="list__address">{warehouse.warehouse_name}</p> <img src={chevron} />
-                                </div>
-                            </Link>
-                            <h2 className="list__header-mobile">Address</h2>
-                            <p className="list__address-text">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
-                            <button className="list__btn-mobile" onClick={() => alert(`id: ${warehouse.id}`)}> 
-                            <img src={deleteBtn} /> 
-                            </button>
-                        </div>
-                        <div className="list__mobile-warehouse">
-                            <h2 className="list__header-mobile">Contact Name</h2>
-                            <p>{warehouse.contact_name}</p>
-                            <h2 className="list__header-mobile">Contact Information</h2>
-                            <p>{warehouse.contact_phone} <br /> {warehouse.contact_email}</p>
-                            <Link to={`/warehouses/edit/${warehouse.id}`} className="list__btn-mobile list__btn-mobile--edit">
-                                <img src={editBtn} />
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="list__warehouse-container">
-                        <Link to={`/warehouses/${warehouseObj.id}`} className="list__warehouse">
-                            <div className="list__icons list__icons--active">
-                                <p className="list__address">{warehouse.warehouse_name}</p> <img src={chevron} />
-                            </div>
-                        </Link>
-                        <p className="list__address">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
-                        <p className="list__name"> {warehouse.contact_name} </p>
-                        <p className="list__info">{warehouse.contact_phone} <br /> {warehouse.contact_email}</p>
-                        <div className="list__list-btns">
-                            
-                            <button onClick={() => alert(`id: ${warehouse.id}`)}>
-                                <img src={deleteBtn} />
-                            </button>
-
-                            <Link to={`/warehouses/edit/${warehouseObj.id}`}>
-                                <img src={editBtn} />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <WarehouseCard 
+                warehouse={warehouse}
+                setDeleteWarehouseOpen={setDeleteWarehouseOpen}
+                setDeleteWarehouseId = {setDeleteWarehouseId}
+                key={warehouse.id}
+                />
             )}
                 )}
             </div>
