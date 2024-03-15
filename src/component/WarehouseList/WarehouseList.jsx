bc4066db0d68c00cabf651aa14a082ceb2a80d7f
Bethany Cannon <bethany.cannon@student.ufv.ca>
2024-03-15 12:22:20 -0700

    make changes to remove global elements

--- src/component/WarehouseList/WarehouseList.jsx
+++ src/component/WarehouseList/WarehouseList.jsx
@@ -14,7 +14,7 @@ function WarehouseList({warehouseData}) {
         <section>
             <div className="list">
                 <div className="list__main-container">
-                    <h1>{warehouseObj.title}</h1>
+                    <h1 className="list__title-text">{warehouseObj.title}</h1>
                     <div className="list__search-container">
                         <input className="list__search" type="text" name="search-bar" placeholder="Search..."></input>
                         <button className="list__search-btn">
@@ -26,18 +26,18 @@ function WarehouseList({warehouseData}) {
 
                 <div className="list__header">
                 <div className="list__icons list__warehouse">
-                    <h2>Warehouse</h2> <img src={sort} alt="sort" />
+                    <h2 className="list__table-header">Warehouse</h2> <img src={sort} alt="sort" />
                 </div>
                 <div className="list__icons list__address">
-                    <h2>Address</h2> <img src={sort} alt="sort" />
+                    <h2 className="list__table-header">Address</h2> <img src={sort} alt="sort" />
                 </div>
                 <div className="list__icons list__name">
-                    <h2>Contact Name</h2> <img src={sort} alt="sort" />
+                    <h2 className="list__table-header">Contact Name</h2> <img src={sort} alt="sort" />
                 </div>
                 <div className="list__icons list__info">
-                    <h2>Contact Information</h2> <img src={sort} alt="sort" />
+                    <h2 className="list__table-header">Contact Information</h2> <img src={sort} alt="sort" />
                 </div>
-                <h2 className="list__action">Actions</h2>
+                <h2 className="list__table-header list__action">Actions</h2>
             </div>
 
         {warehouseData.map((warehouse) => {
