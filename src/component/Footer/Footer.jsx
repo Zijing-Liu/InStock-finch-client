30a6fdac51830a49fe96b1bdd8d2a58308da4940
adams-infinity <lindsayadamsdesign@gmail.com>
2024-03-17 23:27:03 -0400

    revert back to orginal footer styles

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div className='footer__copywrite'>
-            <p3>© InStock Inc. All Rights Reserved.</p3>
+            <p>© InStock Inc. All Rights Reserved.</p>
             </div>
         </footer>
     )
